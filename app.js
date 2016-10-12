a0e1b2a842264543ea92924793e65a0e614fb94d
NoBey <786964300@qq.com>
2016-10-13 02:56:03 +0800

    console

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.get('/', function (req, res) {
 app.use(express.query());
 app.use('/wechat', wechat(config, function (req, res, next) {
   // 微信输入信息都在req.weixin上
+
   var message = req.weixin;
+  console.log(message)
   if (message.FromUserName === 'diaosi') {
     // 回复屌丝(普通回复)
     res.reply('hehe');
