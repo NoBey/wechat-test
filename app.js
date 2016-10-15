00b9fa61fb5d7ced426b8bc54807328c78568bb1
NoBey <786964300@qq.com>
2016-10-16 02:26:16 +0800

    wechat

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ var config = {
 var OAuth = require('wechat-oauth');
 var client = new OAuth('wxc0fe8681f03dc6d6', 'your secret');
 
+var List = wechat.List;
+List.add('view', [
+  ['回复{a}查看我的性别', function (info, req, res) {
+    res.reply('我是个妹纸哟');
+  }],
+  ['回复{b}查看我的年龄', function (info, req, res) {
+    res.reply('我今年18岁');
+  }],
+  ['回复{c}查看我的性取向', '这样的事情怎么好意思告诉你啦- -']
+]);
+
 
 app.get('/', function (req, res) {
   res.send('公众号正在开发测试中!');
@@ -26,16 +37,6 @@ app.use('/wechat', wechat(config, function (req, res, next) {
 
 }));
 
-var List = require('wechat').List;
-List.add('view', [
-  ['回复{a}查看我的性别', function (info, req, res) {
-    res.reply('我是个妹纸哟');
-  }],
-  ['回复{b}查看我的年龄', function (info, req, res) {
-    res.reply('我今年18岁');
-  }],
-  ['回复{c}查看我的性取向', '这样的事情怎么好意思告诉你啦- -']
-]);
 
 var server = app.listen(12345, function () {
   var host = server.address().address;
