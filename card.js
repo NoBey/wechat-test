4ccebe02160a66bef16139864e421a8c556e491d
NoBey <786964300@qq.com>
2016-10-22 23:42:18 +0800

    card

--- card.js
+++ card.js
@@ -1,3 +1,13 @@
 const charset = require('superagent-charset');
 const request = require('superagent');
 charset(request);
+
+request.agent().post('http://202.113.80.18:7777/pls/wwwbks/bks_login2.login?')
+      .type('form')
+      .send({ stuid: '20142510', pwd: '123456' })
+      .charset('gbk')
+      .end((err, res) => {
+        cookies = res.request.cookies
+        console.log(res.request.cookies)
+
+        })
