140060faaa1badb12ff80c12bbadfe6c9120dc7f
NoBey <786964300@qq.com>
2016-10-23 00:26:44 +0800

    card

--- card.js
+++ card.js
@@ -2,12 +2,12 @@ const charset = require('superagent-charset');
 const request = require('superagent');
 charset(request);
 
-request.agent().post('http://202.113.80.18:7777/pls/wwwbks/bks_login2.login?')
-      .type('form')
-      .send({ stuid: '20142510', pwd: '123456' })
+request.get('http://card.tjcu.edu.cn/homeLogin.action')
+      // .type('form')
+      // .send({ "Login.Token1": '20142510', "Login.Token2": '111111' })
       .charset('gbk')
       .end((err, res) => {
-        cookies = res.request.cookies
-        console.log(res.request.cookies)
-
+        // cookies = res.request.cookies
+        // console.log(res.request.cookies)
+        console.log(res.text)
         })
